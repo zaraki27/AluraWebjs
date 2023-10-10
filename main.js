625e98e1676008926271fbdb7ad00eeab14afa48
zaraki27 <108496481+zaraki27@users.noreply.github.com>
2023-10-10 19:23:04 -0300

    mudar o .js

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-alert('Hello os world');
-onclick
+// Funções para reproduzir o som quando um botão é clicado
+function playSound(soundId) {
+    const audioElement = document.getElementById(soundId);
+    if (audioElement) {
+        audioElement.currentTime = 0; // Reinicia o áudio, caso já esteja tocando
+        audioElement.play();
+    }
+}
+
+// Adicione um evento de clique a cada botão
+document.querySelectorAll('.tecla').forEach(button => {
+    button.addEventListener('click', function() {
+        const soundId = 'som_' + this.classList[1]; // Pega a classe do botão para encontrar o ID do áudio
+        playSound(soundId);
+    });
+});
