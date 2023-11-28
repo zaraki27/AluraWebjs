0adc740a050440fe8c268cc363762823a85d8adc
zaraki27 <108496481+zaraki27@users.noreply.github.com>
2023-11-28 19:18:20 -0300

    final

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+# Projeto Base para rotações das estações
 # EfeitosSonorosRadioGremio
 Mesa de mídias com áudios para efeitos sonoros especiais da rádio Grêmio Estudantil
--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-function tocaSomRisada(){
-    document.querySelector("#som_tecla_risada").play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
     
 }
+const listadeTeclas = document.querySelectorAll(".tecla");
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
-
-listaDeTeclas[0].onclick = tocaSomAplausos;
+for (let contador = 0; contador < listadeTeclas.length; contador++){
+    const tecla = listadeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    }
+      
+}
