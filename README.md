8c07208a0d5b1d4a25ebc020c38eebbcab7f8082
zaraki27 <108496481+zaraki27@users.noreply.github.com>
2023-11-28 19:21:15 -0300

    Aula final

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# Projeto Base para rotações das estações
 # EfeitosSonorosRadioGremio
 Mesa de mídias com áudios para efeitos sonoros especiais da rádio Grêmio Estudantil
--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
-    
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        console.log("Elemento não encontrado ou seletor inválido")
+    }
 }
-const listadeTeclas = document.querySelectorAll(".tecla");
 
-for (let contador = 0; contador < listadeTeclas.length; contador++){
+
+for(let contador = 0; contador < listadeTeclas.length; contador++){
     const tecla = listadeTeclas[contador];
     const efeito = tecla.classList[1];
-    const idAudio = `#som_${efeito}`;
+    const idAudio = `#som_${efeito}`; 
+
+    
     tecla.onclick = function(){
         tocaSom(idAudio)
     }
-      
+
+    tecla.onkeydown = function (evento) {        
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');              
+        }      
+            
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
 }
+
